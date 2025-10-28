2a3cc4608935ce31e3a20ff70423a2f1d6801439
Alessandro R. da Silva <alessandrozinco@hotmail.com>
2025-10-28 14:47:47 -0300

    Instalacao adminlte, conf BD, instalacao NPM

---
+++ resources/views/exemplo.blade.php
@@ -0,0 +1,24 @@
+
+@extends('adminlte::page') 
+
+<!--titudo que  aparece na aba do navegador -->
+@section('title','Cadastro Usuário')
+
+
+
+@section('content_headr')
+    <h1>Cadastro Usuário</h1>
+@stop
+@section('content')
+ <!-- Conteudo da pagina --> 
+
+<h1>Aqui vai o coneteúdo da página</h1>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+<script>console.log("HI!")</script>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('exemplo');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
